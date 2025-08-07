9452c4f551b1b998fd222c740c82d20b952648be
Adil Khan <adilmkhan89@gmail.com>
2025-08-07 13:27:08 -0500

    js submission update

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,50 @@
 const editButton = document.querySelector(".profile__edit-btn");
 const editModal = document.querySelector("#edit-profile-modal");
 const editCloseButton = editModal.querySelector(".modal__button-close");
-const editform = editModal.querySelector(".modal__form");
+const profileFormElement = editModal.querySelector(".modal__form");
 const newPostButton = document.querySelector(".profile__add-btn");
 const postModal = document.querySelector("#new-post-modal");
 const postCloseButton = postModal.querySelector(".modal__button-close");
 const postForm = postModal.querySelector(".modal__form");
 
+// Select the necessary form elements. You should select
+// these from inside the modal, not the document.
+const nameInput = profileFormElement.querySelector("#name"); // Use querySelector()
+const jobInput = profileFormElement.querySelector("#description"); // Use querySelector()
+
+// If you haven't done so already, select
+// the profile elements from the document.
+const profileNameElement = document.querySelector(".profile__name"); // Use querySelector()
+const profileJobElement = document.querySelector(".profile__description"); // Use querySelector()
+
+// Create the form submission handler.
+function handleProfileFormSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+
+  // Get the values of each form field from the value
+  // property of the corresponding input element.
+
+  // Insert these new values into the textContent
+  // property of the corresponding profile elements.
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
+
+  // Close the modal.
+  editModal.classList.remove("modal_is-opened");
+}
+
 editButton.addEventListener("click", function () {
   editModal.classList.add("modal_is-opened");
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
 });
 
 editCloseButton.addEventListener("click", function () {
   editModal.classList.remove("modal_is-opened");
 });
 
-editform.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  editModal.classList.remove("modal_is-opened");
-});
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 
 newPostButton.addEventListener("click", function () {
   postModal.classList.add("modal_is-opened");
